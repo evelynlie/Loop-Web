0c2decbca40d0cfffc8689af0dbab4f9a7ad540a
rmit-kent-go <s3955624@student.rmit.edu.au>
2023-09-28 15:28:54 +1000

    fixed movie poster not loading

--- react/src/data/repository.js
+++ react/src/data/repository.js
@@ -1,4 +1,13 @@
 import axios from "axios";
+// Do not delete this section. This host movie poster on react
+import BarbiePoster from '../movie_posters/barbie.jpeg';
+import OppenheimerPoster from '../movie_posters/oppenheimer.jpeg';
+import MissionImpossiblePoster from '../movie_posters/mission_impossible.jpeg';
+import TheMoonPoster from '../movie_posters/the_moon.jpeg';
+import TheMarvelsPoster from '../movie_posters/the_marvels.jpeg';
+import WonkaPoster from '../movie_posters/wonka.jpeg';
+import ConcreteUtopiaPoster from '../movie_posters/concrete_utopia.jpeg';
+import DunePartTwoPoster from '../movie_posters/dune_part_two.jpeg';
 
 // --- Constants ----------------------------------------------------------------------------------
 const API_HOST = "http://localhost:4000";
